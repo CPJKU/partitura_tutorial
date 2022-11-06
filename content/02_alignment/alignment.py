cae2be3f6e108cc826b2a0c1b8e5bf1b65b0e1d7
Carlos Cancino-Chacón <carloscancinochacon@gmail.com>
2022-11-06 22:54:44 +0100

    update for Colab

--- content/02_alignment/alignment.py
+++ content/02_alignment/alignment.py
@@ -1,18 +1,20 @@
+from typing import Union, Tuple
+
 import numpy as np
-from fastdtw import dtw, fastdtw
+from fastdtw import fastdtw
 from scipy.spatial import distance as sp_dist
 
 from scipy.spatial.distance import cdist
 
 
 def pairwise_distance_matrix(
-    X: np.ndarray, 
-    Y: np.ndarray, 
-    metric: str = 'euclidean'
+    X: np.ndarray,
+    Y: np.ndarray,
+    metric: str = "euclidean",
 ) -> np.ndarray:
     """
     Compute pairwise distance matrix of two sequences
-    
+
     Parameters
     ----------
     X : np.ndarray
@@ -20,7 +22,7 @@ def pairwise_distance_matrix(
     Y : np.ndarray
         A 2D array with size (m_observations, n_features)
     metric: str
-        A string defining a metric (see possibilities 
+        A string defining a metric (see possibilities
         in scipy.spatial.distance.cdist)
 
     Returns
@@ -34,6 +36,7 @@ def pairwise_distance_matrix(
         Y = Y.T
     C = cdist(X, Y, metric=metric)
     return C
+
 
 def accumulated_cost_matrix(C: np.ndarray) -> np.ndarray:
     """
@@ -54,16 +57,16 @@ def accumulated_cost_matrix(C: np.ndarray) -> np.ndarray:
     D = np.zeros((N, M))
     D[0, 0] = C[0, 0]
     for n in range(1, N):
-        D[n, 0] = D[n-1, 0] + C[n, 0]
+        D[n, 0] = D[n - 1, 0] + C[n, 0]
     for m in range(1, M):
-        D[0, m] = D[0, m-1] + C[0, m]
+        D[0, m] = D[0, m - 1] + C[0, m]
     for n in range(1, N):
         for m in range(1, M):
-            D[n, m] = C[n, m] + min(D[n-1, m], D[n, m-1], D[n-1, m-1])
+            D[n, m] = C[n, m] + min(D[n - 1, m], D[n, m - 1], D[n - 1, m - 1])
     return D
 
 
-def optimal_warping_path(D):
+def optimal_warping_path(D: np.ndarray) -> np.ndarray:
     """
     Compute the warping path given an accumulated cost matrix
 
@@ -71,7 +74,7 @@ def optimal_warping_path(D):
     ----------
     D: np.ndarray
         Accumulated cost Matrix
-    
+
     Returns
     -------
     P: np.ndarray
@@ -88,27 +91,28 @@ def optimal_warping_path(D):
         elif m == 0:
             cell = (n - 1, 0)
         else:
-            val = min(D[n-1, m-1], D[n-1, m], D[n, m-1])
-            if val == D[n-1, m-1]:
-                cell = (n-1, m-1)
-            elif val == D[n-1, m]:
-                cell = (n-1, m)
+            val = min(D[n - 1, m - 1], D[n - 1, m], D[n, m - 1])
+            if val == D[n - 1, m - 1]:
+                cell = (n - 1, m - 1)
+            elif val == D[n - 1, m]:
+                cell = (n - 1, m)
             else:
-                cell = (n, m-1)
+                cell = (n, m - 1)
         P.append(cell)
         (n, m) = cell
     P.reverse()
     return np.array(P)
 
-def naive_dynamic_time_warping(
-    X: np.ndarray, 
-    Y: np.ndarray, 
-    metric: str ='euclidean',
+
+def dynamic_time_warping(
+    X: np.ndarray,
+    Y: np.ndarray,
+    metric: str = "euclidean",
     return_distance: bool = False,
-) -> (np.ndarray, float):
+) -> Union[np.ndarray, Tuple[np.ndarray, float]]:
     """
     Naive Implementation of Vanilla Dynamic Time Warping
-    
+
     Parameters
     ----------
     X : np.ndarray
@@ -116,14 +120,19 @@ def naive_dynamic_time_warping(
     Y: np.ndarray
         Array Y
     metric: string
-        Scipy Metric
-        
+        Name of the metric to use. See possible metrics in
+        `scipy.spatial.distance`.
+    return_distance : bool
+       Return the dynamic time warping distance.
+
+
     Returns
     -------
     warping_path: np.ndarray
         The warping path for the optimal alignment.
     dtwd : float
         The dynamic time warping distance of the alignment.
+        This distance is only returned if `return_distance` is True.
     """
     # Compute pairwise distance matrix
     C = pairwise_distance_matrix(X, Y, metric=metric)
@@ -132,46 +141,10 @@ def naive_dynamic_time_warping(
     dtwd = D[-1, -1]
     # Get warping path
     warping_path = optimal_warping_path(D)
-    
+
     if return_distance:
         return warping_path, dtwd
     return warping_path
-
-def dynamic_time_warping(
-    X: np.ndarray,
-    Y: np.ndarray,
-    metric: str = "euclidean",
-    return_distance: bool = False,
-) -> (np.ndarray, float):
-    """
-    Vanilla Dynamic Time Warping
-
-    This is a more efficient implementation
-
-    Parameters
-    ----------
-    X : np.ndarray
-    Y: np.ndarray
-    metric : str
-       The name of the metric to use
-
-    Returns
-    -------
-    warping_path: np.ndarray
-        The warping path for the best alignment
-    dtwd : float
-        The dynamic time warping distance of the alignment.
-    """
-    # Get distance measure from scipy dist
-    dist = getattr(sp_dist, metric)
-    dtwd, warping_path = dtw(X, Y, dist=dist)
-
-    # Make warping path a numpy array
-    warping_path = np.array(warping_path)
-
-    if return_distance:
-        return warping_path, dtwd
-    return warping_path, dtwd
 
 
 def fast_dynamic_time_warping(
@@ -179,7 +152,7 @@ def fast_dynamic_time_warping(
     Y: np.ndarray,
     metric: str = "euclidean",
     return_distance: bool = False,
-) -> (np.ndarray, float):
+) -> Union[np.ndarray, Tuple[np.ndarray, float]]:
     """
      Fast Dynamic Time Warping
 
@@ -188,14 +161,19 @@ def fast_dynamic_time_warping(
     Parameters
     ----------
     X : np.ndarray
+        Array X.
     Y: np.ndarray
+        Array Y.
     metric : str
-       The name of the metric to use
+        The name of the metric to use. See possible metrics in
+        `scipy.spatial.distance`.
 
     Returns
     -------
     warping_path: np.ndarray
-        The warping path for the best alignment
+        The warping path for the best alignment. The first column
+        are indices in array `X` and the second column represents
+        the corresponding index in array `Y`.
     dtwd : float
         The dynamic time warping distance of the alignment.
     """
