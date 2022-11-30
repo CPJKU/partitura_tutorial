09df1c215bae7374d9139ab21275583cdde95a01
melkisedeath <manoskaristineos@gmail.com>
2022-11-30 16:18:02 +0100

    change the html theme options test.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,22 @@ html_theme = 'insegel'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = 'assets/partitura_logo_black.png'
+
+html_theme_options = {
+    'analytics_id': 'G-344421322',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 1,
+    'includehidden': True,
+    'titles_only': False
+}
